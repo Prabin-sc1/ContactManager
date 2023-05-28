39d9c40c285b647e695ab91ac3ae2262513434f7
prabin.sc1 <prabin.sc1@gmail.com>
2023-05-28 09:46:09 +0545

    style.css, script.js created and added in base.html

---
+++ SmartContactManager/src/main/java/com/boot/contact/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.boot.contact.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/home")
+	public String home() {
+		return "home";
+	}
+
+	@RequestMapping("/about")
+	public String about() {
+		return "about";
+	}
+}
