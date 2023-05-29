b0d831664d4f375df4dece5cbde3c356bf803fcd
prabin.sc1 <prabin.sc1@gmail.com>
2023-05-29 12:55:29 +0545

    created UserDetailsServiceImpl, CutomUserDetails, MyConfig for login

---
+++ SmartContactManager/src/main/java/com/boot/contact/config/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.boot.contact.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.boot.contact.dao.UserRepository;
+import com.boot.contact.entities.User;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.getUserByUserName(username);
+		if (user == null) {
+			throw new UsernameNotFoundException("Could not found user!");
+		}
+		CutomUserDetails cutomUserDetails = new CutomUserDetails(user);
+		return cutomUserDetails;
+	}
+
+}
